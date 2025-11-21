c9b2233ecd5bb9c4095add410a5ae62f1a21692b
hamzazirari <hamzazirari6002@gmail.com>
2025-11-21 09:52:02 +0100

    terminer le modal dajout dun employer et modal d'informartion du employer

---
+++ main.js
@@ -0,0 +1,102 @@
+const modal = document.getElementById("modal-employer");
+const openBtnModal = document.getElementById("open-modal");
+const closeBtnModal = document.getElementById("close-modal");
+const expContainer = document.getElementById("exp-container");
+const addExpBtn = document.getElementById("add-exp");
+
+
+//OUVERTURE MODAL EMPLOYER................
+openBtnModal.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+});
+
+//FERMETURE MODAL EMPLOYER ....................;
+
+closeBtnModal.addEventListener("click", () => {
+    modal.classList.add("hidden");
+});
+
+
+//BOUTTON DAJOUT DE EXP....................
+
+const addExp = document.getElementById("add-exp");
+
+addExp.addEventListener("click", () => {
+
+    const expDiv = document.createElement("div");
+    expDiv.className = " exp-item mt-2 border p-3 rounded-lg flex flex-col gap-2 "
+
+    const inputPost = document.createElement("input");
+    inputPost.type = "text";
+    inputPost.placeholder = "Poste";
+    inputPost.className = "border p-2 rounded-lg";
+    expDiv.appendChild(inputPost);
+
+    const inputEntreprise = document.createElement("input");
+    inputEntreprise.type = "text";
+    inputEntreprise.placeholder = "Entreprise";
+    inputEntreprise.className = "border p-2 rounded-lg";
+    expDiv.appendChild(inputEntreprise);
+
+    const inputPeriode = document.createElement("input");
+    inputPeriode.type = "text";
+    inputPeriode.placeholder = "Entreprise";
+    inputPeriode.className = "border p-2 rounded-lg";
+    expDiv.appendChild(inputPeriode);
+
+    const inputDescription = document.createElement("textarea");
+    inputDescription.type = "text";
+    inputDescription.placeholder = "Entreprise";
+    inputDescription.className = "border p-2 rounded-lg";
+    expDiv.appendChild(inputDescription);
+
+    const btnDeleteExp = document.createElement("button");
+    btnDeleteExp.className = "text-red-500 self-end";
+    expDiv.appendChild(btnDeleteExp);
+
+    const iconTrash = document.createElement("i");
+    iconTrash.className = "cursor-pointer fa-solid fa-trash";
+    btnDeleteExp.appendChild(iconTrash);
+    btnDeleteExp.addEventListener("click", () => {
+        expDiv.remove();
+    });
+    expContainer.appendChild(expDiv);
+
+});
+
+
+
+//------------Ajouteer------------
+const formEmployer = document.getElementById("form-employer");
+
+formEmployer.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    //------------------------REGEX--------
+
+    const nomComplet = document.getElementById("input-nom");
+    const inputEmail = document.getElementById("input-email");
+    const inputTele = document.getElementById("input-tele");
+
+    let nomRegex = /^[a-zA-Z ]{2,30}$/;
+
+    if (nomRegex.test(nomComplet.value) === false) {
+        alert("Veuiller entrer seulement des caracteres et une espace");
+        return;
+    }
+
+    let emailRegex = /^[^\s\W]{4,}@[^\d\s]{1,5}\.[a-zA-Z]{1,4}$/;
+
+    if (emailRegex.test(inputEmail.value) === false) {
+        alert("Veuiller entrer seulement des caracteres et numero sans espace avant @");
+        return;
+    }
+
+    let teleRegex = /^(06|05|07)\d{8}$/;
+
+    if (teleRegex.test(inputTele.value) === false) {
+        alert("Veuiller entrer seulement des numeros sans espaces");
+        return;
+    }
+
+});
